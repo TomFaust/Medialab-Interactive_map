2ef593ed32786bfaddeeb23b366cb191f337d8f2
Rik <rik.vermeulen.1997@live.nl>
2022-03-22 22:03:46 +0100

    WW-22 | created database connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,25 @@
-const PORT = 8000
 const express = require('express')
 const axios = require('axios')
+const  mongoose = require("mongoose");
+const config = require('dotenv/config')
+
+const PORT = 8000
+const URL = `mongodb+srv://
+    ${process.env.DB_USER}:
+    ${process.env.DB_PASSWORD}@
+    ${process.env.DB_NAME}`
 
 const app = express()
+
+//Connect to DB
+mongoose.connect(URL, { useNewUrlParser: true},() => {
+    console.log('conntected to database')
+});
 
 app.get('/api', (req, res) =>  {
     res.json('Welcome to the API')
 })
 
-app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
+app.listen(PORT, () => 
+    console.log(`Server is running on ${PORT}`
+))
