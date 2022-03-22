35e1871a17032e6bbe3a98c0cbe4d53d2384aa54
tomfaust <tom.faust@hotmail.com>
2022-03-22 19:23:58 +0100

    installed react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Map from 'react-map-gl';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+    const [viewport,setViewport] = useState({
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14,
+        width:"100vw",
+        height:"100vh",
+    })
+
+    return (
+        <div>
+            <Map {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}>test</Map>
+        </div>
+    );
 }
 
-export default App;
