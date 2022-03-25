17c1cb982c47615c90b70efa4dd46cb72d772915
Tom Faust <tom.faust@hotmail.com>
2022-03-25 10:46:25 +0100

    Rendered Mapbox map

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React, {useState} from "react";
-import Map from 'react-map-gl';
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidG9tLWZhdXN0IiwiYSI6ImNsMTI3M2JkbzAzcG8za3BkZG42ZWF6cTEifQ.6QbYKTRSF5IXZKKpXXKyaA';
 
 export default function App() {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
 
-    const [viewport,setViewport] = useState({
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14,
-        width:"100vw",
-        height:"100vh",
-    })
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom
+        });
+    });
 
     return (
         <div>
-            <Map {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}>test</Map>
+            <div className="sidebar">
+                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
+            <div ref={mapContainer} className="map-container" />
         </div>
     );
+
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 ReactDOM.render(
   <React.StrictMode>
