7a235f507bc4fe176dce57ffc3c768133e9052b0
Rik <rik.vermeulen.1997@live.nl>
2022-03-25 15:24:26 +0100

    fixed bug with db connection

--- server/config/globals.js
+++ server/config/globals.js
@@ -5,8 +5,5 @@ config();
 export const env = {
 	NODE_ENV: process.env.NODE_ENV || 'development',
 	NODE_PORT: process.env.NODE_PORT || 8000,
-    NODE_CONNECTION: `mongodb+srv://
-    ${process.env.DB_USER}:
-    ${process.env.DB_PASSWORD}@
-    ${process.env.DB_NAME}`
+    NODE_CONNECTION: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}`
 };
