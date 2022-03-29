5099e932f99a72fc00cdea9aa1e1ab775f12c9cf
Rik <rik.vermeulen.1997@live.nl>
2022-03-29 15:28:14 +0200

    added login token to env file

--- server/config/globals.js
+++ server/config/globals.js
@@ -5,5 +5,6 @@ config();
 export const env = {
 	NODE_ENV: process.env.NODE_ENV || 'development',
 	NODE_PORT: process.env.NODE_PORT || 8000,
-    NODE_CONNECTION: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}`
+    NODE_CONNECTION: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}`,
+	LOGIN_TOKEN: process.env.LOGIN_TOKEN,
 };
