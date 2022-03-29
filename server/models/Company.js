aabd7016e5c139943f02e7179d4eb279c1bba60e
Rik <rik.vermeulen.1997@live.nl>
2022-03-29 23:00:22 +0200

    Updated schema's for company and country

--- server/models/Company.js
+++ server/models/Company.js
@@ -1,6 +1,6 @@
-const  mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const CompanySchema = mongoose.Schema({
+const CompanySchema = new mongoose.Schema({
     name: { 
         type: String,
         required: true,
@@ -21,64 +21,10 @@ const CompanySchema = mongoose.Schema({
         type: Boolean,
         required: true,
     },
-    temperature: { 
-        type: Number,
-        required: true,
-    },
-    hardness: { 
-        type: String,
-        required: true,
-    },
-    turbidity: { 
-        type: String,
-        required: true,
-    },
-    health: {
-        nitrate: { 
-            type: Number,
-            required: true,
-        },
-        nitrite: {
-            type: Number,
-            required: true,
-        },
-        fluoride: {
-            type: Number,
-            required: true,
-        },
-    },
-    taste: {
-        water_extraction_area: {
-            type: String,
-            required: true,
-        },
-        sulfate: {
-            type: Number,
-            required: true,
-        },
-        magnesium: {
-            type: Number,
-            required: true,
-        },
-        natrium: {
-            type: Number,
-            required: true,
-        },
-        chloride: {
-            type: Number,
-            required: true,
-        },
-        magnesiumcarbonate: {
-            type: Number,
-            required: true,
-        },
-        calciumcarbonate: {
-            type: Number,
-            required: true,
-        },
-        chloride: {
-            type: Number,
-            required: true,
-        }
+    date: { 
+        type: Date,
+        default: Date.now,
     }
 })
+
+export default mongoose.model('Company',CompanySchema);
