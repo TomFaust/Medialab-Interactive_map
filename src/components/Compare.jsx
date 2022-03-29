fdbb349e927e11a65b1aecf60156033c1bd2a96e
Niek Rottier <niekrottier@gmail.com>
2022-03-29 22:51:48 +0200

    Styled compare box contents

--- src/components/Compare.jsx
+++ src/components/Compare.jsx
@@ -10,31 +10,46 @@ export function Compare({open, baseData, compareData}) {
         }
     }
 
-    function countryContent(data) {
-        if (isEmptyObject(data)) {
-            return <p className='feedback-msg'>Selecteer een land op de kaart om waterstatistieken te vergelijken.</p>
+    // Check if the compare box is opened
+    if (open) {
+        // Check if all the data is provided
+        if (isEmptyObject(baseData) || isEmptyObject(compareData)) {
+            return(
+                <div className='compare'>
+                    <p className='feedback-msg'>Please select two countries to compare their water</p>
+                </div>
+            )
         } else {
-            return (
-                <div>
-                    <h3>{data.location}</h3>
-                    <p>{data.temp}</p>
-                    <p>{data.hardness}</p>
+            return(
+                <div className='compare'>
+                    <div id="locations">
+                        <h3>{baseData.location}</h3>
+                        <h3>{compareData.location}</h3>
+                    </div>
+
+                    <div className="stats">
+                        <div className='max-stat'>
+                            <div id='base-hardness'>{baseData.hardness}</div>
+                        </div>
+                        <p>Hardness</p>
+                        <div className='max-stat'>
+                            <div id='compare-hardness'>{compareData.hardness}</div>
+                        </div>
+                    </div>
+
+                    <div className="stats">
+                        <div className='max-stat'>
+                            <div id='base-temp'>{baseData.temp}</div>
+                        </div>
+                        <p>Temperature</p>
+                        <div className='max-stat'>
+                            <div id='compare-temp'>{compareData.temp}</div>
+                        </div>
+                    </div>
                 </div>
             )
         }
-    }
-
-    if (open) {
-        return(
-            <div className='compare'>
-                <div className="baseCountry">
-                    {countryContent(baseData)}
-                </div>
-                <div className="compareCountry">
-                    {countryContent(compareData)}
-                </div>
-            </div>
-        )
+        
     } else {
         return null
     }
