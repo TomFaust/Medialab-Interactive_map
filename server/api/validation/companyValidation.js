ddd2d87d75b409528a0bca2ac1b11a9a6cd57ae3
Qwinsie <theqpowerrr@gmail.com>
2022-03-30 14:55:27 +0200

    added routes and validation for companies.

---
+++ server/api/validation/companyValidation.js
@@ -0,0 +1,24 @@
+import joi from '@hapi/joi'
+
+
+function countryValidation(data) {
+
+    const schemaCountryValidation = joi.object({
+        name: joi.string()
+        .min(6)
+        .required(),
+        country: joi.string()
+        .min(6)
+        .required(),
+        longitude: joi.number()
+        .min(6)
+        .required(),
+        latitude: joi.number()
+        .min(6)
+        .required(),
+    })
+
+    return schemaCountryValidation.validate(data);
+}
+
+export default countryValidation
