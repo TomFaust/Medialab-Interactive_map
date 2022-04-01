37b9ceb4f2525ea23f5289b834a69c70d1b92ad3
Rik <rik.vermeulen.1997@live.nl>
2022-04-01 21:06:07 +0200

    updated validation

--- server/api/validation/waterPropertiesValidation.js
+++ server/api/validation/waterPropertiesValidation.js
@@ -8,6 +8,57 @@ function WaterPropertiesValidation(data) {
         .required(),
         period: joi.string()
         .required(),
+        temperature: [{
+            value: joi.number()
+            .min(0)
+            .max(25)
+        }],
+        hardness: [{
+            value: joi.number()
+            .min(1.1)
+            .max(2)
+        }],
+        turbidity: [{
+            value: joi.number()
+            .min(0)
+            .max(4)
+        }],
+        health: [{
+            nitrate: [{
+                value: joi.number()
+                .min(0)
+                .max(50)
+            }],
+            nitrite: [{
+                value: joi.number()
+                .min(0)
+                .max(0.1)
+            }],
+            fluoride: [{
+                value: joi.number()
+                .min(0)
+                .max(1.1)
+            }],
+        }],
+        taste: [{
+            water_extraction_area: joi.string()
+            .valid('Groundwater','Surface water','Dune water'),
+            sulfate: [{
+                value: joi.number()
+                .min(0)
+                .max(150)
+            }],
+            natrium: [{
+                value: joi.number()
+                .min(0)
+                .max(150)
+            }],
+            chloride: [{
+                value: joi.number()
+                .min(0)
+                .max(150)
+            }],
+        }],
     })
 
     return schemaWaterPropertiesValidation.validate(data);
